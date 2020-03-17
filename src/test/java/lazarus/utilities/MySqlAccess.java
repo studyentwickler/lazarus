7141ac5829c2fa5479e64688fd7b9ad8504a7289
Alex <study.entwickler@yandex.ru>
2020-03-17 11:34:08 +0100

    fix

--- src/test/java/lazarus/utilities/MySqlAccess.java
+++ src/test/java/lazarus/utilities/MySqlAccess.java
@@ -9,33 +9,28 @@ public class MySqlAccess {
 
     protected static Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass());
 
+
     public void readDataBase() throws Exception {
 
         String path = TestConstants.get_database_credentials();
         String user = Authentication.dataFromTxtFile(path).get("Login");
         String password = Authentication.dataFromTxtFile(path).get("Password");
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
-        private Connection connect = null;
-        private Statement statement = null;
-        private PreparedStatement preparedStatement = null;
-        private ResultSet resultSet = null;
+        Class.forName("com.mysql.jdbc.Driver");
+        connect = DriverManager
+                .getConnection("jdbc:mysql://localhost/test_constants?"
+                        + "user=" + user + "&password=" + password);
+        statement = connect.createStatement();
+        resultSet = statement
+                .executeQuery("select * from test_constants");
+        logger.info(resultSet);
 
-        try {
+        assert resultSet != null;
+        resultSet.close();
 
-            Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager
-                    .getConnection("jdbc:mysql://localhost/test_constants?"
-                            + "user=" + user + "&password=" + password);
-            statement = connect.createStatement();
-            resultSet = statement
-                    .executeQuery("select * from test_constants");
-            logger.info(resultSet);
-
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            connect.close();
-        }
     }
-
 }
